9f5b417e519f924ca7cc146c2f280bb67a333043
Adriano Kaito <adriano.kaito@gmail.com>
2022-04-21 19:17:12 -0300

    Reactive Form example

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    });
+    // ValueChanges:
+    /* this.signupForm.valueChanges.subscribe(
+      (value) => console.log(value)
+    ); */
+
+    // StatusChanges:
+    this.signupForm.statusChanges.subscribe(
+      (stat) => console.log(stat)
+    );
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    this.signupForm.reset({
+      'gender': 'female'
+    });
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      // Simular uma chamada a um webservice e aguardar a resposta
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+}
